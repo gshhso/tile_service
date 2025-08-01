0eb3515381d4f4cc886c041fe03ffc63eeca0dfb
gshhso <oooupoyuan@gmail.com>
2025-08-02 01:44:07 +0800

    first commit

---
+++ tile_service/tile.py
@@ -0,0 +1,75 @@
+import rioxarray as rxr
+import xarray as xr
+import numpy as np
+import rasterio
+import geopandas as gpd
+import mercantile
+import pickle
+
+from geopandas.sindex import SpatialIndex as SIndex
+from shapely.geometry import box
+from pathlib import Path
+from typing import cast
+from rioxarray import merge
+from functools import lru_cache
+
+Pathlike = str | Path
+BBOX = tuple[float, float, float, float]
+XYZ = tuple[int, int, int]
+
+
+def get_slice(bbox) -> dict:
+    return {"x": slice(bbox[0], bbox[2]), "y": slice(bbox[3], bbox[1])}
+
+
+def build_rtree_index(
+    image_list: list[Pathlike],
+    save_root: Pathlike,
+    luotu_name: str = "luotu.geojson",
+    index_name: str = "index.pkl",
+):
+    save_root = Path(save_root)
+    bounds = []
+    for image in image_list:
+        with rasterio.open(image) as src:
+            bounds.append(box(*src.bounds))
+    luotu = gpd.GeoDataFrame({"path": image_list, "geometry": bounds}, crs="EPSG:4326")  # type: ignore
+    with open(save_root / index_name, "wb") as f:
+        pickle.dump(luotu.sindex, f)
+    with open(save_root / luotu_name, "w") as f:
+        luotu.to_file(f)
+
+
+def load_rtree_index(
+    save_path: Pathlike, luotu_path: Pathlike
+) -> tuple[gpd.GeoDataFrame, SIndex]:
+    with open(save_path, "rb") as f:
+        sindex = pickle.load(f)
+    with open(luotu_path, "r") as f:
+        gdf = gpd.read_file(f)
+    return gdf, sindex
+
+
+def filter_intersect_image(
+    luotu: gpd.GeoDataFrame, sindex: SIndex, bbox: BBOX
+) -> list[Pathlike]:
+    query = box(*bbox)
+    filtered_luotu = luotu.iloc[luotu.sindex.query(query, predicate="intersects")]
+    filtered_luotu = cast(gpd.GeoSeries, filtered_luotu)
+    return filtered_luotu["path"].to_list()
+
+
+@lru_cache(maxsize=None)
+def load_dataarray(path: Pathlike) -> xr.DataArray:
+    return cast(xr.DataArray, rxr.open_rasterio(path))
+
+def load_dataarrays(root: Pathlike) -> xr.DataArray:
+    image_list = list(Path(root).rglob("**/*.tif"))
+    dataarrays = [load_dataarray(image) for image in image_list]
+    return merge.merge_arrays(dataarrays)
+
+def get_tiles(dataarray: xr.DataArray, bbox: BBOX) -> np.ndarray:
+    return dataarray.sel(**get_slice(bbox)).to_numpy()
+
+def convert_xyz_to_bbox(xyz: XYZ) -> BBOX:
+    return mercantile.bounds(xyz[0], xyz[1], xyz[2])
